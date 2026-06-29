bb84ac59ab7950d5c0fa68903910c086e5287839
Philippe Bouteille <PHILIPPEBOUTEILLE37@GMAIL.COM>
2026-06-29 01:19:50 -0400

    Create page.tsx

---
+++ app/[locale]/quienes-somos/page.tsx
@@ -0,0 +1,13 @@
+import { getPagina, PaginaBloques } from "@/components/PaginaBloques";
+import type { Locale } from "@/lib/i18n";
+
+export const revalidate = 3600;
+
+export default async function LocaleQuienesSomosPage({
+  params,
+}: {
+  params: { locale: Locale };
+}) {
+  const pagina = await getPagina("quienes-somos");
+  return <PaginaBloques pagina={pagina} locale={params.locale} />;
+}
