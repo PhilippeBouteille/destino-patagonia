56acc9a4eadbbb7ec2dc1627a111c87d607796bc
Philippe Bouteille <PHILIPPEBOUTEILLE37@GMAIL.COM>
2026-06-28 22:44:37 -0400

    Create icons.ts

---
+++ lib/icons.ts
@@ -0,0 +1,9 @@
+const BASE = "https://destinopatagonia.cl/wp-content/uploads/2019/12";
+
+export const ICONOS_CATEGORIA: Record<string, string> = {
+  express: `${BASE}/lancha1-LSR.png`,
+  osada: `${BASE}/icono-osado.png`,
+  istmo: `${BASE}/icono-osado.png`,
+  "kayak-medio-dia": `${BASE}/icono-kayak.png`,
+  "kayak-6-dias": `${BASE}/icono-camping-experto.png`,
+};
