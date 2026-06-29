44f0f4af04f06ebc4d9697aa0f530c9aeee429c4
Philippe Bouteille <PHILIPPEBOUTEILLE37@GMAIL.COM>
2026-06-28 22:11:02 -0400

    Update page.tsx

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from "@/lib/supabase/server";
+import { createClient } from "@/lib/supabase/public";
 import type { InfoGeneral } from "@/lib/types";
 
 export const revalidate = 3600;
